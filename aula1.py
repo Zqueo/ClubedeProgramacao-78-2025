a2b4b6c2122290838185c54d3b9fcf0f45ad30e3
Zaqueu Silva <58602244+Zqueo@users.noreply.github.com>
2025-08-14 15:00:42 -0300

    Create aula1.py

---
+++ aula1.py
@@ -0,0 +1,20 @@
+print('Hello World')
+
+nome = 'zaqueu'
+idade = 27
+turma = 'A'
+
+print('O meu nome é ' + nome)
+print('Eu tenho ' + str(idade) + ' anos')
+print('Minha turma é ' + turma)
+print()
+print()
+
+num1 = 10
+num2 = 5
+num3 = 5
+num4 = 10
+
+resultado = (num1 + num2+ num3 +num4) /4
+
+print(resultado) 
