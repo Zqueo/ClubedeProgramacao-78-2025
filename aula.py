5ca1aceb81e8a9ef8fa4b2660a6315e870d1baa8
Zaqueu Silva <58602244+Zqueo@users.noreply.github.com>
2025-08-21 15:00:35 -0300

    Create aula.py

---
+++ aula.py
@@ -0,0 +1,15 @@
+print ('Hello World')
+
+nota1 = input('Digite o primeiro número:')
+nota2 = input('Digite o segundo número:')
+nota3 = input('Digite o terceiro número:')
+nota4 = input('Digite o querto número:')
+
+nota1 = int(nota1)
+nota2 = int(nota2)
+nota3 = int(nota3)
+nota4 = int(nota4)
+
+res = (nota1 + nota2 + nota3 + nota4)/4
+
+if res >= 6 : print('Aluno Aprovado')
