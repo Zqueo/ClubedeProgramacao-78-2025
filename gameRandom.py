ae05d2e2fcda8beb1539b4c73532e296ea85fe86
Zaqueu Silva <queubh98@gmail.com>
2025-08-27 15:05:02 -0300

    alt

--- gameRandom.py
+++ gameRandom.py
@@ -5,7 +5,7 @@ import os
 tentativa = 0
 palpite = 0
 a=1 #limite inicial
-b=10 #limite final
+b=100 #limite final
 
 #ESCOLHA DO NUM ALEATORIO
 num = random.randint(a,b)
@@ -13,13 +13,16 @@ num = random.randint(a,b)
 #JOGO
 print('JOGO DE ADIVINHAÇÃO \n\n')
 print(f'Tente adivinhar o número escolhido ({a} a {b})')
+input('Pressione para continuar...')
 while 1:
+    os.system('cls')  # Limpa a tela a cada tentativa
     tentativa += 1
     print ('Tentativa ' + str(tentativa))
     palpite = int(input('Digite o seu palpite: '))
 
-    if palpite > 10 or palpite < 1 :
+    if palpite > b or palpite < a:
         print ('palpite inválido')
+        input('Pressione para continuar...')
         continue
     elif palpite > num :
         print ('palpite maior que o número')
@@ -28,9 +31,7 @@ while 1:
     elif palpite == num :
         print ('parabens!!! você acertou')
         break
-    
     input('Pressione para continuar...')
-    os.system('cls')  # Limpa a tela a cada tentativa
 
 print (f"Você acertou em {tentativa} tentativas")
 input('Pressione enter para sair...')
